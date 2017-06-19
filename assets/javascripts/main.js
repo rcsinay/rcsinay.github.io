f45713f37cc87b183226c74294fb0dd1a0543224
Gerard Montemayor <grbmontemayor@gmail.com>
2017-06-19 20:07:52 +0800

    Finish website

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
 			// Only prevent default if animation is actually gonna happen
 			event.preventDefault();
 			$('html, body').animate({
-			scrollTop: target.offset().top
+			scrollTop: target.offset().top - 80
 			}, 500, function() {
 			// Callback after animation
 			// Must change focus!
